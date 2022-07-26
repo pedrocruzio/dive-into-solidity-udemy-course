150ed702469b0087afd097ab3e47aff37faab2e0
pedrocruzio <pedro@rokailabs.com>
2022-07-26 12:24:02 -0400

    finished knowledge test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,40 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    modifier onlyOwner {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address account) external {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool result, ) = account.call{value: getBalance()}("");
+        require(result, "TX_FAILED");
+    }
+
+    function start() external {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string memory s1, string memory s2) public pure returns (string memory) {
+        return string(abi.encodePacked(s1,s2));
+    }
+
 }
